b6e0342f91f5094ccfb7d0c51af04fb658284a5a
sujeethkurapati <sujeethpintu@gmail.com>
2021-07-03 00:20:36 -0400

    New changes for branch

---
+++ Dev/Python Scripts/test.py
@@ -0,0 +1,17 @@
+# i = 5
+# print(pow(sum([pow(10,item)for item in range(0,i)]),2))
+lst =[2,1]
+alst = [[1,2],[2,3],[3,4]]
+
+if lst in alst:
+    print('yes')
+
+# def fact(n):
+#     val = 1 if n == 1 else n * fact(n-1)
+#     return val
+
+# def arrange(lst):
+
+
+# num = fact(len(lst))
+# print(num)
---
+++ Dev/README.md
@@ -0,0 +1,2 @@
+# Dev
+Hi all, this repo of mine contains all the pet projects I have  developed as part of my learning experience  :)
