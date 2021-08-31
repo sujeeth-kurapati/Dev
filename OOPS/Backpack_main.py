2ecccd4a241b3ea7d9d01008b673442ff13078df
sujeethkurapati <sujeethpintu@gmail.com>
2021-08-30 22:47:45 -0400

    Nailing down the core concepts of Classes FTW!!!

---
+++ OOPS/Backpack_main.py
@@ -0,0 +1,6 @@
+class Backpack:
+
+    def __init__(self, color, size):
+        self.items = []
+        self.color = color
+        self.size = size
--- OOPS/main.py
+++ OOPS/main.py
@@ -3,3 +3,26 @@ class House:
     def __init__(self,price,items):
         self.price = price
         self.items = items
+
+class Circle:
+
+    def __init__(self, radius, color = "Blue"):
+        self.radius = radius
+        self.color = color
+
+
+class Rectangle:
+
+    def __init__(self, length, width, color = "Red"):
+        self.length = length
+        self.width = width
+        self.color = color
+
+
+class Movie:
+
+    def __init__(self, title, year, language, rating):
+        self.title = title
+        self.year = year
+        self.language = language
+        self.rating = rating
