93da2622bbd0a452d9a0d375d6653b7261bf5560
sujeethkurapati <sujeethpintu@gmail.com>
2021-09-02 22:50:10 -0400

    Learned Encapsulation, Abstraction and Getters & Setters concepts!!!

--- OOPS/Backpack_main.py
+++ OOPS/Backpack_main.py
@@ -3,8 +3,8 @@ class Backpack:
     max_num_items = 10 # class attribute
 
     def __init__(self, color, size):
-        self.items = []
-        self.color = color
+        self._items = []  #making item attribute as private
+        self.__color = color #Concept of name mangaling -- change the name of the attribute
         self.size = size
  
 
@@ -15,3 +15,7 @@ your_backpack = Backpack("Red", "Large")
 print(Backpack.max_num_items)
 print(my_backpack.max_num_items)
 print(your_backpack.max_num_items)
+
+#print(my_backpack.color) --> This does not work since we are changing the name
+
+print(my_backpack._Backpack__color)
--- OOPS/main.py
+++ OOPS/main.py
@@ -4,14 +4,12 @@ class House:
         self.price = price
         self.items = items
 
+
 class Circle:
 
     def __init__(self, radius, color = "Blue"):
         self.radius = radius
         self.color = color
-        print(self)
-        for item in self:
-            print(item)
 
 
 class Rectangle:
@@ -23,15 +21,62 @@ class Rectangle:
 
 
 class Movie:
-
+    
+    id_counter = 1
     def __init__(self, title, year, language, rating):
-        self.title = title
+        self._id = Movie.id_counter
+        self._title = title
         self.year = year
         self.language = language
         self.rating = rating
 
+        Movie.id_counter += 1
+
+    def get_title(self): # created a seperate function to call the non public variable. this is the functionality of the getter
+        return self._title
+
+
+class Dog:
+
+    def __init__(self, name, age):
+        self._name = name
+        self.age = age
+
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, new_name): # Defined a setter function for updating the attribute value
+        if isinstance(new_name, str): # we can addsome check conditions before assigning the values
+            self._name = new_name
+        else:
+            print("Please enter a valid name for the Dog")
+
+
+class Car:
+
+    def __init__(self, brand, model, year):
+        self.brand = brand
+        self.model = model
+        self._year = year   #by adding under score we declared it as an private variable
+
 
 cir = Circle(8)
 print(cir.radius)
-# for item in cir:
-#     print(item)
+
+
+my_car = Car("Porsche", "911 Turbo S", 2021)
+print(my_car._year)
+ 
+
+my_movie = Movie("Pride and Prejudice", 2010, "English", 7.7)
+your_movie = Movie("Tenet", 2020, "English", 8.4)
+
+print(my_movie._id)
+print(my_movie.get_title()) #Using the functionality of getter
+
+my_dog = Dog("Nemo", 3)
+print(my_dog.get_name())
+print("Now we are updating the name of the dog")
+my_dog.set_name("Flash")
+print(my_dog.get_name())
+my_dog.set_name(99) #if we pass a value other than string it will not be assigned since we are checking for the data type
