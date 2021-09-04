faaf9d9f0c8d3661099d174fd5cbb720eed8481a
sujeethkurapati <sujeethpintu@gmail.com>
2021-09-03 20:06:25 -0400

    Worked on the implementation of 'Property' functionality

--- OOPS/main.py
+++ OOPS/main.py
@@ -7,9 +7,35 @@ class House:
 
 class Circle:
 
+    VALID_COLORS = ("Red", "Green", "Blue", "Black") # we have considered this a tuple data structure since it is immutable
+
     def __init__(self, radius, color = "Blue"):
-        self.radius = radius
-        self.color = color
+        self._radius = radius
+        self._color = color
+
+    def get_radius(self):
+        print("Getter is being called")
+        return self._radius
+
+    def set_radius(self, new_radius):
+        print("Setter is being called")
+        if isinstance(new_radius, int) and new_radius > 0:
+            self._radius = new_radius
+        else:
+            print("Please valid value for the radius of the circle")
+
+    radius = property(get_radius, set_radius) # Safe way of calling Non - Public variables with getter and setter implementation 
+
+    def get_color(self):
+        return self._color
+
+    def set_color(self, new_color):
+        if new_color in Circle.VALID_COLORS:
+            self._color = new_color
+        else:
+            print("Please enter a valid color")
+
+    color = property(get_color, set_color)
 
 
 class Rectangle:
@@ -59,21 +85,24 @@ class Car:
         self.model = model
         self._year = year   #by adding under score we declared it as an private variable
 
+print(50*"*","Circle Class print variables",50*"*")
+cir = Circle(8, "Red")
+print(cir.radius) #here we dont need to update the attribute syntax to call the private variables since we are implementing "property" functionality
+print(cir.color)
+cir.radius += 1
+print(cir.radius) 
 
-cir = Circle(8)
-print(cir.radius)
-
-
+print(50*"*","Car Class print variables",50*"*")
 my_car = Car("Porsche", "911 Turbo S", 2021)
 print(my_car._year)
  
-
+print(50*"*","Movie Class print variables",50*"*")
 my_movie = Movie("Pride and Prejudice", 2010, "English", 7.7)
 your_movie = Movie("Tenet", 2020, "English", 8.4)
-
 print(my_movie._id)
 print(my_movie.get_title()) #Using the functionality of getter
 
+print(50*"*","Dog Class print variables",50*"*")
 my_dog = Dog("Nemo", 3)
 print(my_dog.get_name())
 print("Now we are updating the name of the dog")
