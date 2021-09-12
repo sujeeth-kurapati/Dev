80fb3594e75f7daefa3d14ec0b049fc49c0eeda0
sujeethkurapati <sujeethpintu@gmail.com>
2021-09-11 22:00:50 -0400

    Started working on Tic-Tac-Toe Game

---
+++ OOPS/Projects/tic_tac_toe/player.py
@@ -0,0 +1,31 @@
+import random
+class Player:
+
+    def __inti(self, marker='X', is_human=True):
+        self._marker = marker
+        self._is_human = is_human
+
+    @property
+    def marker(self):
+        return self._marker
+
+    @property
+    def is_human(self):
+        return self._is_human
+
+    def get_player_move(self, input_location):
+        if self._is_human:
+            return self.get_human_move()
+        else:
+            return self.get_computer_move()
+
+    def get_human_move(self):
+        move = input("Please enter the player's move : ")
+        return move
+
+    def get_computer_move(self):
+        row = random.choice([1, 2, 3])
+        column = random.choice(["A", "B", "C"])
+        move = str(row) + column
+        print("Computer's Move : ", move)
+        return move
