d7a3d955a2d0ccc4e0431450a9b7a23e0f31e5e9
sujeethkurapati <sujeethpintu@gmail.com>
2022-03-04 19:58:49 -0500

    added api functionality to Lambda and updated SAM template

--- AWS/SAM/src/app.py
+++ AWS/SAM/src/app.py
@@ -1,4 +1,10 @@
 import json
 
 def lambda_handler(event, context):
-    return "Hello this lambda was deployed from SamTemplate"
+    # return "Hello this lambda was deployed from SamTemplate"
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "hello world",
+        }),
+    }
