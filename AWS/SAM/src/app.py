bd79241be41bf5cf6d8988622e82bc541d1c9927
sujeethkurapati <sujeethpintu@gmail.com>
2022-03-04 21:55:48 -0500

    added api functionality to Lambda, API, Dynamodb and updated SAM template

--- AWS/SAM/src/app.py
+++ AWS/SAM/src/app.py
@@ -1,10 +1,20 @@
 import json
+import pprint
+import os
+import boto3
+
+table_name = os.environ['TABLE_NAME']
+rgn = os.environ['REGION_NAME']
+dynam = boto3.client('dynamodb',region_name = rgn)
 
 def lambda_handler(event, context):
     # return "Hello this lambda was deployed from SamTemplate"
+    pprint.pprint(event)
+    scan_results = dynam.scan(TableName=table_name)
+    pprint.pprint(scan_results)
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
+            "message": "Hello, I am Sujeeth :)",
         }),
     }
