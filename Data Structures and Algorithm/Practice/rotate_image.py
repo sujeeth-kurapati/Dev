dfb6f76c6f1f58af8f0104468e639338d0b1b2ed
sujeethkurapati <sujeethpintu@gmail.com>
2022-06-20 19:40:44 -0400

    implemented rotate image

--- Data Structures and Algorithm/Practice/rotate_image.py
+++ Data Structures and Algorithm/Practice/rotate_image.py
@@ -1,22 +1,16 @@
 def rotate_matrix(arr, n):
-    l,t = 0,0
-    r = b = n
-    # while l < r and t < b:
-    for i in range(l, r-1):
-        for j in range(t,b-1):
-            print(i,j)
-            first = arr[i][j]
-            tmp = arr[i][r-1-j]
-            arr[i][r-1-j] = first
-            scnd_tmp = arr[b-1-i][r-1-j]
-            arr[b-1-i][r-1-j] = tmp
-            third_tmp = arr[b-1-i][j]
-            arr[b-1-i][j] = scnd_tmp
-            arr[i][j] = third_tmp
 
-    return arr
-
+    # traonspose matrix
+    for i in range(0, n):
+        for j in range(0,n):
+            if j<=i:
+                arr[i][j], arr[j][i] = arr[j][i], arr[i][j]
+            
+    for i in range(0,n):
+        for j in range(0,n//2):
+            arr[i][j],arr[i][n-1-j] = arr[i][n-j-1],arr[i][j] 
+    print(arr)
 
 arr = [[1,2,3],[4,5,6],[7,8,9]]
 arr_len = len(arr)
-print(rotate_matrix(arr, arr_len))
+rotate_matrix(arr, arr_len)
